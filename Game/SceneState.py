c3f63aa6a42cb1d64bdf573f485158245ca509f5
Tree <keithmaccuish5@gmail.com>
2023-02-28 15:15:10 -0700

    SceneState

---
+++ Game/SceneState.py
@@ -0,0 +1,22 @@
+from Trengine.SM.State import State
+
+
+class SceneState(State):
+    def __init__(self, name, scene) -> None:
+        super().__init__(name)
+        self.scene = scene
+
+    def on_enter(self):
+        pass
+
+    def on_exit(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        pass
