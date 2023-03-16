d44d490ca8f18ebbffa1d3afa5fdc19273c3539d
Tree <keithmaccuish5@gmail.com>
2023-03-15 19:11:12 -0600

    fixing imports

--- Game/__init__.py
+++ Game/__init__.py
@@ -1,5 +1,5 @@
 from .GameScene import GameScene
 from .SceneState import SceneState
-from .Spritesheet import Spritesheet
-from .Tiles import Tiles
+from .Spritesheet import SpriteSheet
+from .Tiles import *
 from .TreeGame import TreeGame
