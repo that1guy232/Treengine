39ec5aafc8b0b51201caa5aa77713fdb1b3907fc
Tree <keithmaccuish5@gmail.com>
2023-03-18 21:38:21 -0600

    UI - UIButton

--- UI/Button.py
+++ UI/Button.py
@@ -1,67 +1,19 @@
-# x, y, width, height, color, text, callback
+
+from .UIWidget import UIWidget
+
 import pygame
+from typing import Tuple
 
-
-class Button:
-
-    """
-    x (int): The x-coordinate of the top-left corner of the button.
-    y (int): The y-coordinate of the top-left corner of the button.
-    width (int): The width of the button in pixels.
-    height (int): The height of the button in pixels.
-    color (tuple): The color of the button in RGB format.
-    text (str): The text that will be displayed on the button.
-    callback (function): The function that will be called when the button is clicked.
-    """
-
-    def __init__(self, x, y, width, height, color, text, callback):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+class Button(UIWidget):
+    def __init__(self, position: Tuple[int, int], size: Tuple[int, int], color: Tuple[int, int, int], enabled: bool = True) -> None:
+        super().__init__(position, enabled)
+        self.size = size
         self.color = color
-        self.text = text
-        self.callback = callback
+        self.rect = pygame.Rect(position, size)
 
-        ########################
+    def handle_event(self, event: pygame.event.Event) -> None:
+        if event.type == pygame.MOUSEMOTION:
+            self.is_hovered = self.rect.collidepoint(event.pos)
 
-        """TODO: We should not be creating a new font every time we create a button
-        Solution: UI Manager?
-        Does each scene have its own UI manager?
-        How does the UI mananger know what UI elements to handle?
-        Does the UI Manager create the UI elements?        
-        
-        """
-        self.font = pygame.font.SysFont("comicsans", 20)
-
-        ########################
-
-        self.hidden = False
-
-    def draw(self, surface):
-        if not self.hidden:
-            # draw the text
-            text = self.font.render(self.text, 1, (0, 0, 0))
-            pygame.draw.rect(
-                surface, self.color, (self.x, self.y, self.width, self.height)
-            )
-            surface.blit(
-                text,
-                (
-                    self.x + (self.width / 2 - text.get_width() / 2),
-                    self.y + (self.height / 2 - text.get_height() / 2),
-                ),
-            )
-
-    def is_over(self, pos):
-        if self.x < pos[0] < self.x + self.width:
-            if self.y < pos[1] < self.y + self.height:
-                return True
-        return False
-
-    def handle_event(self, event):
-        if not self.hidden:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    if self.is_over(event.pos):
-                        self.callback()
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.rect(screen, self.color, self.rect)
