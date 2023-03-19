35bbb2fd96b20ba5a8dd08eb1d776490b1e02326
Tree <keithmaccuish5@gmail.com>
2023-03-19 00:01:24 -0600

    docs

--- UI/TextLabel.py
+++ UI/TextLabel.py
@@ -3,6 +3,11 @@ from .UIWidget import UIWidget
 from typing import Tuple
 
 class TextLabel(UIWidget):
+    """_summary_
+
+    Args:
+        UIWidget (_type_): _description_
+    """    
     def __init__(
         self,
         position: Tuple[int, int],
@@ -11,6 +16,15 @@ class TextLabel(UIWidget):
         color: Tuple[int, int, int],
         enabled: bool = True,
     ) -> None:
+        """_summary_
+
+        Args:
+            position (Tuple[int, int]): _description_
+            font (pygame.font.Font): _description_
+            text (str): _description_
+            color (Tuple[int, int, int]): _description_
+            enabled (bool, optional): _description_. Defaults to True.
+        """        
         super().__init__(position, enabled)
         self.font = font
         self.text = text
